a9499b4dd42a84e73da311be4d201bb506f9904a
jhalzel <jeremy123jeremy@gmail.com>
2023-07-16 01:23:02 -0400

    Removed unnecessary files

--- src/script.py
+++ src/script.py
@@ -29,17 +29,7 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
-# Access credentials from the dictionary
-try:
-    SP_API_REFRESH_TOKEN = os.environ["SP_API_REFRESH_TOKEN"]
-    LWA_APP_ID = os.environ["LWA_APP_ID"]
-    LWA_CLIENT_SECRET = os.environ["LWA_CLIENT_SECRET"]
-    SP_API_ACCESS_KEY = os.environ["SP_API_ACCESS_KEY"]
-    SP_API_SECRET_KEY = os.environ["SP_API_SECRET_KEY"]
-    SP_API_ROLE_ARN = os.environ["SP_API_ROLE_ARN"]
-except KeyError:
-    logger.error("Please set the environment variables!")
-    raise
+
 
 # Access credentials from the dictionary
 credentials = {
