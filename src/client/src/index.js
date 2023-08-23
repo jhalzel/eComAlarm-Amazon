abe3868639af5686a1d1ee8fd639eabfc0ae28e2
jhalzel <jeremy123jeremy@gmail.com>
2023-08-22 21:59:57 -0400

    changed fonts

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Gugi&family=Sarpanch:wght@500&display=swap');
+    </style>
   </React.StrictMode>
 );
 
