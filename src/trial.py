2917f7bc8043e1fc2aa77b666dabdf7ffc8aaa65
jhalzel <jeremy123jeremy@gmail.com>
2023-09-05 18:33:36 -0400

    modified members

--- src/trial.py
+++ src/trial.py
@@ -124,34 +124,34 @@ def members():
     total_order_count = status['total_order_count']  # Extract the order count value from the dictionary
     last_updated = status['last_updated']  # Extract the last updated timestamp value from the dictionary
     
-    json_data = jsonify({'fba_sales': fba_sales, 
-                    'fbm_sales': fbm_sales, 
-                    'total_order_count': total_order_count, 
-                    'order_pending_count': order_pending_count, 
-                    'last_updated': last_updated, 
-                    'shipped_order_count': shipped_order_count, 
-                    'total_sales': total_sales,
-                    'fba_pending_sales': fba_pending_sales,
-                    'fbm_pending_sales': fbm_pending_sales,
-                    'fbm_threshold': fbm_threshold
-                    })
+    # json_data = jsonify({'fba_sales': fba_sales, 
+    #                 'fbm_sales': fbm_sales, 
+    #                 'total_order_count': total_order_count, 
+    #                 'order_pending_count': order_pending_count, 
+    #                 'last_updated': last_updated, 
+    #                 'shipped_order_count': shipped_order_count, 
+    #                 'total_sales': total_sales,
+    #                 'fba_pending_sales': fba_pending_sales,
+    #                 'fbm_pending_sales': fbm_pending_sales,
+    #                 'fbm_threshold': fbm_threshold
+    #                 })
 
-    # # Create a dictionary containing the data
-    # data_dict = {
-    #     'fba_sales': fba_sales,
-    #     'fbm_sales': fbm_sales,
-    #     'total_order_count': total_order_count,
-    #     'order_pending_count': order_pending_count,
-    #     'last_updated': last_updated,
-    #     'shipped_order_count': shipped_order_count,
-    #     'total_sales': total_sales,
-    #     'fba_pending_sales': fba_pending_sales,
-    #     'fbm_pending_sales': fbm_pending_sales,
-    #     'fbm_threshold': fbm_threshold  # Assuming fbm_threshold is defined elsewhere
-    # }
+    # Create a dictionary containing the data
+    data_dict = {
+        'fba_sales': fba_sales,
+        'fbm_sales': fbm_sales,
+        'total_order_count': total_order_count,
+        'order_pending_count': order_pending_count,
+        'last_updated': last_updated,
+        'shipped_order_count': shipped_order_count,
+        'total_sales': total_sales,
+        'fba_pending_sales': fba_pending_sales,
+        'fbm_pending_sales': fbm_pending_sales,
+        'fbm_threshold': fbm_threshold  # Assuming fbm_threshold is defined elsewhere
+    }
 
-    # # Serialize the data to JSON
-    # json_data = json.dumps(data_dict, indent=4)
+    # Serialize the data to JSON
+    json_data = json.dumps(data_dict, indent=4)
 
     try: 
         # Write the JSON data to 'event.json' file
@@ -160,7 +160,7 @@ def members():
     except FileNotFoundError:
         return jsonify({'error': 'Event file not found'}), 404
     
-    return 
+    return json_data
 
 if __name__ == '__main__':
     app.run()
