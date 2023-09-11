4735a374a7bb1919e8efa49ce80f93b4eb067fac
jhalzel <jeremy123jeremy@gmail.com>
2023-09-10 23:16:36 -0400

    edited CORS

--- src/trial.py
+++ src/trial.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 
 # Enable CORS for all routes
-CORS(app, origins=["http://localhost:3000", "https://amazon-ecom-alarm.onrender.com", "https://rainbow-branch--ecom-alarm.netlify.app/"])
+CORS(app, origins=["http://localhost:3000", "https://amazon-ecom-alarm.onrender.com", "https://rainbow-branch--ecom-alarm.netlify.app"])
 
 # Store the fbm_threshold value
 fbm_threshold = None
