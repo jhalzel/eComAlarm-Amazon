a9bf86d88ad491875abbea7a760eff7f02e27629
jhalzel <jeremy123jeremy@gmail.com>
2023-09-22 21:17:59 -0400

    missing variable fix

--- src/script.py
+++ src/script.py
@@ -381,6 +381,7 @@ def main():
         'total_order_count': [order_count],
         'order_pending_count': [order_pending_count],
         'shipped_order_count': [shipped_order_count],
+        'fba_pending_sales': [round(fba_pending_sales,2)],
         'fbm_pending_sales': [round(fbm_pending_sales,2)],
         'total_sales': [round(fbm_sales, 2) + round(fba_sales, 2)],
         'threshold': [round(threshold, 2)],
